df000f3f4c3eb8d5fcf6f26474cf18def14b0e0a
Daisuke Maeda <dmdragon@mtg.biglobe.ne.jp>
2021-06-10 21:18:39 +0900

    Modified.

--- usr/local/bin/vpnip.sh
+++ usr/local/bin/vpnip.sh
@@ -4,30 +4,23 @@ ipdecimal=/usr/local/lib/ipdecimal.sh
 openvpn_conf=/etc/openvpn/dmdragon.d/dmdragon.conf
 webroot=/home/azureuser/my-first-static-web-app
 
-. $ipdecimal
-
 vpn_network=$(grep ^server $openvpn_conf)
 vpn_network_address=$(echo $vpn_network | cut -d" " -f2)
 vpn_netmask=$(echo $vpn_network | cut -d" " -f3)
 
-echo $vpn_network_address
-echo $vpn_netmask
-
 declare -a addresses=$(ip address | grep inet | awk '{print $2}' | cut -d/ -f1)
 
+. $ipdecimal
 for address in ${addresses[@]}; do
-    echo $address
-    echo $(cat $webroot/index.html)
-    address=192.168.160.2
+    address=192.160.160.2
     if $(ipwith $vpn_network_address $vpn_netmask $address) && [ $address != $(cat $webroot/index.html) ]; then
-        echo ok!
-        # echo $address > $webroot/index.html
-        # pushd $webroot
-        # git add index.html
-        # git commit -m "Change IP Address."
-        # git push origin main
-        # popd
-        # break
+        echo $address > $webroot/index.html
+        pushd $webroot
+        git add index.html
+        git commit -m "Change IP Address."
+        git push origin main
+        popd
+        break
     fi
 done
 
