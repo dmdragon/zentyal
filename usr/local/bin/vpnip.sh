edd438c4f4eacf8b0f5b7c80b01d6791c9dfda22
Daisuke Maeda <dmdragon@mtg.biglobe.ne.jp>
2021-06-10 20:22:04 +0900

    Publish the VPN network's own IP address on Web.

---
+++ usr/local/bin/vpnip.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ipdecimal=/usr/local/bin/ipdecimal.sh
+openvpn_conf=/etc/openvpn/dmdragon.d/dmdragon.conf
+webroot=/home/azureuser/my-first-static-web-app
+
+. $ipdecimal
+
+vpn_network=$(grep ^server $openvpn_conf)
+vpn_network_address=$(echo $vpn_network | cut -d" " -f2)
+vpn_netmask=$(echo $vpn_network | cut -d" " -f3)
+
+declare -a addresses=$(ip address | grep inet | awk '{print $2}' | cut -d/ -f1)
+
+for address in ${addresses[@]}; do
+    echo $address
+    echo $(cat $webroot/index.html)
+    if [ $address ne $(cat $webroot/index.html) ] && [ ipwith $vpn_network_address $vpn_netmask $address ]; then
+        # echo $address > $webroot/index.html
+        # pushd $webroot
+        # git add index.html
+        # git commit -m "Change IP Address."
+        # git push origin main
+        # popd
+        break
+    fi
+done
+
+exit 0
