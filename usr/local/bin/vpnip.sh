84409330223e1ee15375fad6bdbb8be15ab418dc
Daisuke Maeda <dmdragon@mtg.biglobe.ne.jp>
2021-06-10 21:54:37 +0900

    The fix is complete.

--- usr/local/bin/vpnip.sh
+++ usr/local/bin/vpnip.sh
@@ -4,6 +4,7 @@ ipdecimal=/usr/local/lib/ipdecimal.sh
 openvpn_conf=/etc/openvpn/dmdragon.d/dmdragon.conf
 webroot=/home/azureuser/my-first-static-web-app
 
+# vpn_network=$(grep ^server $openvpn_conf)
 vpn_network=$(sudo grep ^server $openvpn_conf)
 vpn_network_address=$(echo $vpn_network | cut -d" " -f2)
 vpn_netmask=$(echo $vpn_network | cut -d" " -f3)
@@ -12,7 +13,6 @@ declare -a addresses=$(ip address | grep inet | awk '{print $2}' | cut -d/ -f1)
 
 . $ipdecimal
 for address in ${addresses[@]}; do
-    address=192.168.160.3
     if $(ipwith $vpn_network_address $vpn_netmask $address) && [ $address != $(cat $webroot/index.html) ]; then
         echo $address > $webroot/index.html
         pushd $webroot
